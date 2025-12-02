94721483139584fd74306ae1e62fb51841109740
huolongteng <hlt@bistu.edu.cn>
2025-12-02 17:21:32 +0800

    Init.

---
+++ read_matadata.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+filename = 'matadata/bodmas.npz'
+data = np.load(filename)
+X = data['X']  # all the feature vectors
+y = data['y']  # labels, 0 as benign, 1 as malicious
+
+print(X.shape, y.shape)
+num = 0
+for i in range(len(y)):
+    if y[i] == 1:
+        print(i)
+        num += 1
+
+    if num == 10:
+        break
